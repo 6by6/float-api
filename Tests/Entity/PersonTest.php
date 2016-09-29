496bf07f913303887360c97c2261fb2f2614635f
daniel <daniel@sixbysix.co.uk>
2016-09-29 08:14:18 +0100

    Added person hydration tests

--- Tests/Entity/PersonTest.php
+++ Tests/Entity/PersonTest.php
@@ -10,4 +10,25 @@ class PersonTest extends AbstractEntityTest
     {
         return Person::class;
     }
+
+    /**
+     * @test
+     */
+    public function hydrate()
+    {
+        /** @var string $json */
+        $json = file_get_contents(__DIR__ . '/json/person.json');
+
+        /** @var array $data */
+        $data = json_decode($json, true);
+
+        /** @var Person $person */
+        $person = Person::deserialize($data);
+
+        $this->assertEquals(299, $person->getId());
+        $this->assertEquals("Abel Anderson", $person->getName());
+        $this->assertEquals("Designer", $person->getJobTitle());
+        $this->assertEquals("avatar-299", $person->getAvatarFile());
+        $this->assertArra([["name" => "Creative", "id" => 4]], $person->getDepartments());
+    }
 }
