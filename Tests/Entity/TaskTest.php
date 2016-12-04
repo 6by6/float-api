d80b6c3c4e5b77e6be093082ae9d8cf1c6b7cb53
Daniel Ness <daniel@sixbysix.co.uk>
2016-12-04 16:32:33 +0000

    Added save/edit/update tests to Task class

--- Tests/Entity/TaskTest.php
+++ Tests/Entity/TaskTest.php
@@ -2,8 +2,10 @@
 
 namespace SixBySix\Float\Tests\Entity;
 
+use SixBySix\Float\Entity\Person;
 use SixBySix\Float\Entity\Project;
 use SixBySix\Float\Entity\Task;
+use SixBySix\Float\Exception\EntityNotFoundException;
 
 class TaskTest extends AbstractEntityTest
 {
@@ -31,21 +33,69 @@ class TaskTest extends AbstractEntityTest
         $this->assertEquals("See related ticket.", $task->getTaskNotes());
     }
 
+
+
     /**
      * @test
      */
-    public function filter()
+    public function save()
     {
-        $task = Task::query()->where('$x ==> $x->getTaskName() == "Write Yesterday"')->first();
-        $this->assertInstanceOf(Task::class, $task);
+        /** @var Person $person */
+        $person = Person::query()->where('$x ==> $x->getName() == "George"')->first();
+
+        /** @var Project $project */
+        $project = Project::query()->where('$x ==> $x->getProjectName() == "White Album"')->first();
+
+        $task = new Task();
+        $task->setTaskName("Write 'While My Guitar Gently Weeps'");
+        $task->setPerson($person);
+        $task->setProject($project);
+
+        $date = new \DateTime();
+        $date->modify('next Tuesday');
+
+        $task->setStartDate($date);
+        $task->setEndDate($date);
+        $task->setHoursPerDay(4);
+        $task->save();
+
+
+        $this->assertInternalType("int", $task->getId());
     }
 
     /**
-     * @_test
+     * @test
      */
-    public function save()
+    public function update()
     {
-        $task = new Task();
-        $task->setTaskName("Write 'While My Guitar Gently Weeps'");
+        /** @var Task $task */
+        $task = Task::query()->first('$x ==> $x->getTaskName() == "Write \'While My Guitar Gently Weeps\'"');
+
+        /** @var int $taskId */
+        $taskId = $task->getId();
+
+        $task->setTaskName("Write 'Long, Long, Long'");
+        $task->setHoursPerDay(5);
+        $task->save();
+
+        $task = Task::getById($taskId);
+        $this->assertEquals("Write 'Long, Long, Long'", $task->getTaskName());
+        $this->assertEquals(5, $task->getHoursPerDay());
+    }
+
+    /**
+     * @test
+     */
+    public function delete()
+    {
+        /** @var Task $task */
+        $task = Task::query()->first('$x ==> $x->getTaskName() == "Write \'Long, Long, Long\'"');
+
+        $taskId = $task->getId();
+
+        $task->delete();
+
+        $this->expectException(EntityNotFoundException::class);
+        Task::getById($taskId);
     }
 }
